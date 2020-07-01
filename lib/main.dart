b129f92e4469152063fbddb4f2341942a5665757
ptyagicodecamp <ptyagicodecamp@gmail.com>
2020-07-01 00:01:46 -0700

    chapter16 -dynamic routing example

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pragmatic_flutter/chapter16/main_16_static.dart';
+import 'package:pragmatic_flutter/chapter16/main_16_dynamic.dart';
 
 void main() => runApp(BooksApp());
