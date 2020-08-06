641ef3fb7237ebf21e2a11840a1917464c39b3c0
ptyagicodecamp <ptyagicodecamp@gmail.com>
2020-08-05 22:51:40 -0700

    updated chaper08 code

--- lib/chapter08/main_08.dart
+++ lib/chapter08/main_08.dart
@@ -13,7 +13,12 @@ class BooksApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BooksListing(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Books Listing"),
+        ),
+        body: BooksListing(),
+      ),
     );
   }
 }
@@ -24,71 +29,55 @@ List bookData() {
     {'title':'Book Title 2','authors':['Author1'], 'image':'assets/book_cover.png'}
   ];
 }
-class BooksListing extends StatefulWidget {
-  @override
-  _BooksListingState createState() => _BooksListingState();
-}
+class BooksListing extends StatelessWidget {
 
-class _BooksListingState extends State<BooksListing> {
-  var booksListing;
-
-  fetchBooks() async {
-    setState(() {
-      booksListing = bookData();
-    });
-  }
+  final booksListing = bookData();
 
   @override
   Widget build(BuildContext context) {
-    fetchBooks();
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Books Listing"),
-      ),
-      body: ListView.builder(
-        itemCount: booksListing == null ? 0 : booksListing.length,
-        itemBuilder: (context, index) {
-          return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            elevation: 5,
-            margin: EdgeInsets.all(10),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Flexible(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          '${booksListing[index]['title']}',
-                          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                        ),
-                        booksListing[index]['authors'] != null
-                            ? Text(
-                          'Author(s): ${booksListing[index]['authors'].join(", ")}',
-                          style: TextStyle(fontSize: 14),
-                        )
-                            : Text(""),
-                      ],
-                    ),
+    return ListView.builder(
+      itemCount: booksListing == null ? 0 : booksListing.length,
+      itemBuilder: (context, index) {
+        return Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          elevation: 5,
+          margin: EdgeInsets.all(10),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        '${booksListing[index]['title']}',
+                        style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                      ),
+                      booksListing[index]['authors'] != null
+                          ? Text(
+                        'Author(s): ${booksListing[index]['authors'].join(", ")}',
+                        style: TextStyle(fontSize: 14),
+                      )
+                          : Text(""),
+                    ],
                   ),
-                  booksListing[index]['image'] != null
-                      ? Image.asset(
-                    booksListing[index]['image'],
-                    fit: BoxFit.fill,
-                  )
-                      : Container(),
-                ],
-              ),
+                ),
+                booksListing[index]['image'] != null
+                    ? Image.asset(
+                  booksListing[index]['image'],
+                  fit: BoxFit.fill,
+                )
+                    : Container(),
+              ],
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
