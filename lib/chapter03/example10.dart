724998fd7cdff6745b00a8bb58c3bd9b0dd8a492
ptyagicodecamp <ptyagicodecamp@gmail.com>
2020-08-25 23:46:09 -0700

    added chapter03-example10 code

---
+++ lib/chapter03/example10.dart
@@ -0,0 +1,18 @@
+//Accessing Stream using `listen`
+
+void printStreamEventsUsingListen() {
+  //Stream is created
+  Stream stream = Stream.fromIterable([1, 2, 3, 4, 5]);
+  print("Stream Starting");
+  stream.listen(
+    (s) => print(s),
+  );
+  print("Stream Finished");
+}
+//----END----//
+
+//Entry point function
+void main() {
+  //Run Example#1.
+  printStreamEventsUsingListen();
+}
