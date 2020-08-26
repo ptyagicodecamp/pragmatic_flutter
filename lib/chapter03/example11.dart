335d2cce3ff0cbc8c2ff170d2157d2cc95a823c6
ptyagicodecamp <ptyagicodecamp@gmail.com>
2020-08-25 23:52:27 -0700

    added chapter03-example11 code

---
+++ lib/chapter03/example11.dart
@@ -0,0 +1,18 @@
+//Example#2. Accessing Stream using `await for`
+
+void printStreamEventsUsingAwaitFor() async {
+  Stream stream = Stream.fromIterable([1, 2, 3, 4, 5]);
+
+  print("Stream Starting");
+  await for (var num in stream) {
+    print(num);
+  }
+  print("Stream Finished");
+}
+//----END----//
+
+//Entry point function
+void main() {
+  //Run Example#2.
+  printStreamEventsUsingAwaitFor();
+}
